bf352a8fbef74108772a8635f0802888b4d1efbf
rahuldkjain <rahuldkjain@gmail.com>
2020-07-15 20:03:57 +0530

    live

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Footer = () => {
+    return (
+        <div className="footer">
+            Developed with <span role="img" aria-label="love">❤️</span> by <a href="https://rahuldkjain.github.io" style={{textDecoration: 'none'}}>Rahul</a>
+        </div>
+    )
+}
+export default Footer;
