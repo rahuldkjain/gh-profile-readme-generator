f51f8b33367114846229d732d3c074a30d617916
rahuldkjain <rahuldkjain@gmail.com>
2020-07-15 20:45:12 +0530

    Updates

---
+++ component---src-pages-404-js-0d3b604ced4c3a37ea39.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("vrFN");t.default=function(){return l.a.createElement("div",null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0d3b604ced4c3a37ea39.js.map
